b9da2d1ff7c66f435869a7c2d7b92aa4185f2a01
Zares Tng <tngzhexuan@gmail.com>
2023-07-17 02:21:38 +0800

    modified some styling for homescreen
    couldnt get the audio to work

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -99,9 +99,9 @@ const HomeScreen = ({ navigation }) => {
       <View>
         <View style={styles.categoryTitleWithArrow}>
             <Text style={styles.categoryTitle}>Start your day</Text>
-            <View style={styles.arrowContainer}>
+            {/* <View style={styles.arrowContainer}>
                 <MaterialIcons name="arrow-forward-ios" size={15} color="black" style={styles.arrowIcon} />
-            </View>
+            </View> */}
         </View>
         <ScrollView horizontal>
             <TouchableOpacity style={styles.containerButton} onPress={handleContainerPressToMeditation}>
@@ -119,9 +119,9 @@ const HomeScreen = ({ navigation }) => {
       <View>
       <View style={styles.categoryTitleWithArrow}>
             <Text style={styles.categoryTitle}>Analytics</Text>
-            <View style={styles.arrowContainer}>
+            {/* <View style={styles.arrowContainer}>
                 <MaterialIcons name="arrow-forward-ios" size={15} color="black" style={styles.arrowIcon} />
-            </View>
+            </View> */}
         </View>
         <ScrollView horizontal>
             <TouchableOpacity style={styles.containerButton} onPress={handleContainerPressToDiaryAnalytics}>
